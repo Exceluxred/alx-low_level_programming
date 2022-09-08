7fc06e9297ae273720e725239092075ce5134710
Exceluxred <excelartmusic@gmail.com>
2022-09-08 14:40:07 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * * main - this is the strating point of the program
- * * Return: returns 0
- * */
+ * main - this is the strating point of the program
+ * Return: returns 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
