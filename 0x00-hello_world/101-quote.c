8e0de4eb871b0d9e302690bf66a9437f794bd4b0
Exceluxred <excelartmusic@gmail.com>
2022-09-08 14:52:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include<unistd.h>
 
 /**
- * * main - the main function
- * * Return: returns 1
- * */
+*main - the main function
+*Return: returns 1
+*/
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
