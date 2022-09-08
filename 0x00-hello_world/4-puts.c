fd851f2ebb1c41282564469cf715948d29fdd26b
Exceluxred <excelartmusic@gmail.com>
2022-09-08 14:28:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: returns 0
  */
 int main(void)
