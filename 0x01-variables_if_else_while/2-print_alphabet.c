34d3680ef55ff602ffc761d5781820ab21b3c5c0
Exceluxred <excelartmusic@gmail.com>
2022-09-12 00:07:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - the main function
+ *Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+		putchar('\n');
+	return (0);
+}
