1378d6c7ef2315fe3b33cd1e9b7f030b43a8abf1
Exceluxred <excelartmusic@gmail.com>
2022-09-12 00:12:52 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ *main - two loops that print the alphabet lower and upper case
+ *Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+		putchar('\n');
+	return (0);
+}
