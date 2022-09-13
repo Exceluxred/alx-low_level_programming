7571caff49f335229c4a814ccfbab9fb5d2dcaee
Exceluxred <excelartmusic@gmail.com>
2022-09-13 08:23:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,9 @@
-#include "main.h"
-
-/**
- *main - prints the string _putchar with headers
- *Return: returns 0
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-}
+#include <stdio.h> 
+ /* added std out put 
+  * added std input 
+  * set the main to be void 
+  */ 
+ int main (void) 
+ { 
+         printf ("_putchar\n"); 
+         return (0);  }
