5a9814bedcd4f6f6e9a233a57c56078a9a1fd255
Exceluxred <excelartmusic@gmail.com>
2022-09-13 06:21:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - prints the string _putchar with headers
+ *Return: returns 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints the alphabet in lowercase
+ *Return: returns nothing
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 97; alpha <= 122; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
