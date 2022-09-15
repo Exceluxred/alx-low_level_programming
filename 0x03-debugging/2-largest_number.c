b1d1f9d34f1878b4813340f96a977c381f236909
Exceluxred <excelartmusic@gmail.com>
2022-09-15 02:43:26 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
-#include "main.h"
-#include "excel.h"
+#include "holberton.h"
+
 /**
- *largest_number - returns the largest of 3 numebrs
+ *largest_number - returns the largest of 3 numbers
  *@a: first integer
  *@b: second integer
  *@c: third integer
@@ -24,5 +24,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
+
 	return (largest);
 }
