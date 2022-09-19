6fc4952d684762060326ec71b702701582ffbd9f
Exceluxred <excelartmusic@gmail.com>
2022-09-19 07:13:08 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *puts2 - a function that prints every other character of
+ *a string, followed by a new line, to stdout
+ *@str: the string of characters to be printed
+ *Return: returns nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; 1 ; i++)
+	{
+		if (*(str + i) == 0)
+		{
+			printf("\n");
+			break;
+		}
+		if (i % 2 == 0)
+			printf("%c", *(str + i));
+	}
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_array - a function that prints n elements
+ *of an array of integers, followed by a new line
+ *@a: the array to be printed
+ *@n: the number of elements to print from the array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n != 0 && n != 1)
+		for (i = 0; i < n; i++)
+		{
+			if (i != (n - 1))
+				printf("%d, ", a[i]);
+			else if (i == (n - 1))
+				printf("%d", a[i]);
+		}
+	printf("\n");
+}
