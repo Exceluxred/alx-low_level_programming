b699417f11bbb6587363a7fe4acd5418443f4a5a
Exceluxred <excelartmusic@gmail.com>
2022-10-05 17:13:16 -0700

    free_grid - frees a 2 dimensional grid/array

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,14 @@
 
 void free_grid(int **grid, int height)
 {
-	int row;
+	int i;
 
-	for (row = 0; row < height; row++)
-		free(grid[row]);
+	if (grid == NULL || height <= 0)
+		return;
 
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
 	free(grid);
 }
