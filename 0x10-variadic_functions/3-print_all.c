4f5b78f53a814f0f2c601a4fa88ecda4d281d4b1
Exceluxred <excelartmusic@gmail.com>
2022-10-13 08:54:40 -0700

    variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,52 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- *print_all - a function that prints anything
- *@format: a list of types of arguments passed to the function
- *Return: returns nothing
+ *print_all - prints anything
+ *@format: list of types of arguments passed to the function
  */
+
 void print_all(const char * const format, ...)
 {
-	int i = 0, j = 0;
-	va_list args;
-	char *ptr;
+	va_list valist;
+	unsigned int i = 0, middle = 0;
+	char *arg;
 
-	va_start(args, format);
-	while (j < 1 && format != NULL)
-		while (format[i] != '\0')
-		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(args, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					break;
-				case 's':
-					ptr = va_arg(args, char *);
-					if (ptr == NULL)
-						ptr = "(nil)";
-					printf("%s", ptr);
-					break;
-				default:
-					i++;
-					j++;
-					continue;
-			}
-			j++;
-			if (format[i + 1] == '\0')
-			{
-				break;
-			}
+	va_start(valist, format);
+
+	while (format && format[i])
+	{
+		if (middle)
 			printf(", ");
-			i++;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(valist, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(valist, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double));
+				break;
+			case 's':
+				arg = va_arg(valist, char *);
+				if (arg)
+				{
+					printf("%s", arg);
+					break;
+				}
+				printf("%p", arg);
+				break;
+			default:
+				middle = 0;
+				i++;
+				continue;
 		}
+		middle = 1;
+		i++;
+	}
 	printf("\n");
-	va_end(args);
+	va_end(valist);
 }
